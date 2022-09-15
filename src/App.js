4319c62685ea4f219dbf074287280543e3b1c4ee
Xavier Castaner <xavier.castaner@moodys.com>
2022-09-15 11:54:54 +0200

    Add giphy endpoint

--- src/App.js
+++ src/App.js
@@ -7,14 +7,10 @@ function App() {
 export default App;
 
 /**
-We want to do an application where we will show a joke of Chuck Norris fact random
-(https://api.chucknorris.io/jokes/random) and generate an AI image with this joke.
+We want to do an application where we will show a joke of Chuck Norris fact randomly
+(https://api.chucknorris.io/jokes/random) and generate a gif with 3 first words of the joke on giphy api.
+(https://api.giphy.com/v1/gifs/search?api_key=FmakFDGCtcHiqGBRR50rUIK0w4dAvMyb&q={query}&limit=25&offset=0&rating=g&lang=en)
 
-This is the curl command to generate the image:
-curl \
-    -F 'text=YOUR_TEXT_URL' \
-    -H 'api-key:quickstart-QUdJIGlzIGNvbWluZy4uLi4K' \
-    https://api.deepai.org/api/text2img 
 
-For the design we want the joke in the middle of the screen left and the image in the next to the right
+For the design we want the joke in the middle of the screen left and the gif in the next to the right
 */
